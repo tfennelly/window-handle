bcce7108a4fec10b40ba44bbc3849798075b4412
tfennelly <tom.fennelly@gmail.com>
2015-05-22 18:27:18 +0100

    call callback on timeout (with error)

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 var theWindow;
+var defaultTimeout = 10000;
 var callbacks = [];
-var windowSetTimeout;
+var windowSetTimeouts = [];
 
-exports.getWindow = function(callback, timeout) {
-    function execCallback(windowToPass) {
-		if (callback) {
-            try {
-                callback(windowToPass);                
-            } catch (e) {
-                console.log("Error invoking window-handle callback.");
-                console.log(e);
-            }
+function execCallback(callback, theWindow) {
+    if (callback) {
+        try {
+            callback.call(callback, theWindow);                
+        } catch (e) {
+            console.log("Error invoking window-handle callback.");
+            console.log(e);
         }
     }
+}
+
+/**
+ * Get the global "window" object.
+ * @param callback An optional callback that can be used to receive the window asynchronously. Useful when
+ * executing in test environment i.e. where the global window object might not exist immediately. 
+ * @param timeout The timeout if waiting on the global window to be initialised.
+ * @returns {*}
+ */
+exports.getWindow = function(callback, timeout) {
     
 	if (theWindow) {
-        execCallback(theWindow);
+        execCallback(callback, theWindow);
         return theWindow;
 	} 
 	
 	try {
 		if (window) {
-            execCallback(window);
+            execCallback(callback, window);
 			return window;
 		} 
 	} catch (e) {
@@ -30,21 +39,46 @@ exports.getWindow = function(callback, timeout) {
 	}
 
 	if (callback) {
-		callbacks.push(callback);
-		windowSetTimeout = setTimeout(function() {
-			throw "Timed out waiting on the window to be set.";
-		}, (timeout?timeout:10000));
+        function waitForWindow(callback) {
+            callbacks.push(callback);
+            var windowSetTimeout = setTimeout(function() {
+                callback.error = "Timed out waiting on the window to be set.";
+                callback.call(callback);
+            }, (timeout?timeout:defaultTimeout));
+            windowSetTimeouts.push(windowSetTimeout);
+        }
+        waitForWindow(callback);
 	} else {
 		throw "No 'window' available. Consider providing a 'callback' and receiving the 'window' async when available. Typically, this should only be the case in a test environment.";
 	}
 }
 
+/**
+ * Set the global window e.g. in a test environment.
+ * <p>
+ * Once called, all callbacks (registered by earlier 'getWindow' calls) will be invoked.
+ * 
+ * @param newWindow The window.
+ */
 exports.setWindow = function(newWindow) {
-	if (windowSetTimeout) {
-		clearTimeout(windowSetTimeout);
+	for (var i = 0; i < windowSetTimeouts.length; i++) {
+		clearTimeout(windowSetTimeouts[i]);
 	}
+    windowSetTimeouts = [];
 	theWindow = newWindow;
 	for (var i = 0; i < callbacks.length; i++) {
-		callbacks[i](theWindow);
+		execCallback(callbacks[i], theWindow);
 	}
+    callbacks = [];
+}
+
+/**
+ * Set the default time to wait for the global window to be set.
+ * <p>
+ * Default is 10 seconds (10000 ms).
+ * 
+ * @param millis Milliseconds to wait for the global window to be set.
+ */
+exports.setDefaultTimeout = function(millis) {
+    defaultTimeout = millis;
 }
--- spec/index-spec.js
+++ spec/index-spec.js
@@ -7,7 +7,7 @@ var testUtil = require("./test-util");
 
 describe("index.js", function () {
 
-    it("- test", function (done) {
+    it("- test ok", function (done) {
         testUtil.onPage(function(window) {
             var windowHandle = require("../index");
 
@@ -17,6 +17,22 @@ describe("index.js", function () {
             }, 1000);
 
             windowHandle.setWindow(window);
+        });
+    });
+
+    it("- test fail", function (done) {
+        testUtil.onPage(function(window) {
+            var windowHandle = require("../index");
+
+            windowHandle.setWindow(undefined);
+            windowHandle.getWindow(function(theWindow) {
+            	expect(theWindow).not.toBeDefined();
+            	expect(this.error).toBe('Timed out waiting on the window to be set.');
+            	done();
+            }, 1000);
+
+            // don't set the window ... getWindow should timeout, the callback should be
+            // but without a window instance, and ann error message set on the callback.
         });
     });
 });
